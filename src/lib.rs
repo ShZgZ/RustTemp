16623ae083ebc0cb954a0d30c1145d234307b88c
ShZgZ <s.shogo.z89@gmail.com>
2018-08-15 01:20:39 +0900

    add or_gate

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,23 @@
 pub mod gate;
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn and_gate_test() {
+    fn and_gate_tests() {
         assert_eq!(gate::and_gate(0, 0), 0);
         assert_eq!(gate::and_gate(1, 0), 0);
         assert_eq!(gate::and_gate(0, 1), 0);
         assert_eq!(gate::and_gate(1, 1), 1);
+    }
+
+    #[test]
+    fn or_gate_tests() {
+        assert_eq!(gate::or_gate(0, 0), 0);
+        assert_eq!(gate::or_gate(1, 0), 1);
+        assert_eq!(gate::or_gate(0, 1), 1);
+        assert_eq!(gate::or_gate(1, 1), 1);
     }
 /*
     #[test]
